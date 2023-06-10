088494f23149a128b81cd02479cb88a87c323771
Diaz1401 <diaznura@outlook.com>
2023-06-10 13:55:22 +0700

    download-resources: apply custom patch
    Signed-off-by: Diaz1401 <diaznura@outlook.com>

--- download-resources.sh
+++ download-resources.sh
@@ -13,12 +13,14 @@ download() {
     git clone -b dev https://github.com/facebook/zstd zstd
   else
     git clone -b binutils-2_40-branch --depth=1 git://sourceware.org/git/binutils-gdb.git binutils
-    git clone -b releases/gcc-13 --depth=1 git://gcc.gnu.org/git/gcc.git gcc
+    git clone -b releases/gcc-13 --depth=1 https://github.com/Diaz1401/gcc-13 gcc
     git clone -b v1.5.5 https://github.com/facebook/zstd zstd
   fi
   sed -i '/^development=/s/true/false/' binutils/bfd/development.sh
   cd gcc
   ./contrib/download_prerequisites
+  cd ../binutils
+  git apply -3 ../patch/compilespeed-binutils.patch || true
   cd ..
 }
 
